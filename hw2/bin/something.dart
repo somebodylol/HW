73cc61518d5405cd245049bb4bc23df4f15e9bd9
somebodylol <talantbekovadiana71@gmail.com>
2022-03-17 13:26:16 +0600

    hws

--- hw2/bin/something.dart
+++
@@ -1,9 +0,0 @@
-// void main(List<String> arguments) {
-//   var age = 17;
-//   var name = 'Diana';
-//   print('Имя: ${name.toUpperCase()} Возраст: ${age.runtimeType}');
-//   var point = 5.5;
-//   point++;
-//   print(point);
-  
-// }
